38406ef82d8485151e92ea1b433fde2415e191b2
Truong Ngoc De <detruong2002@gmail.com>
2025-06-13 14:13:56 +0700

    SanityWithData

---
+++ app/(client)/page.tsx
@@ -0,0 +1,11 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import  Container  from "../../components/Container";
+import HomeBanner from "@/components/HomeBanner";
+
+export default function Home() {
+  return (
+    <Container className="bg-shop_light_pink">
+      <HomeBanner />
+    </Container>
+  );
+}
---
+++ sanity/schemaTypes/blockContentType.ts
@@ -0,0 +1,41 @@
+import { defineArrayMember, defineType } from "sanity";
+import {ImageIcon} from "@sanity/icons"
+
+export const blockContentType = defineType({
+  title: "Block Content",
+  name: "blockContent",
+  type: "array",
+  of: [
+    defineArrayMember({
+      type: "block",
+      styles: [
+        { title: "Normal", value: "normal" },
+        { title: "H1", value: "h1" },
+        { title: "H2", value: "h2" },
+        { title: "H3", value: "h3" },
+        { title: "H4", value: "h4" },
+      ],
+      lists: [{ title: "Bullet", value: "bullet" }],
+      marks: {
+        decorators: [
+          { title: "Strong", value: "strong" },
+          { title: "Emphasis", value: "em" },
+        ],
+        annotations: [
+          {
+            title: "URL",
+            name: "link",
+            type: "object",
+            fields: [{ title: "URL", name: "href", type: "url" }],
+          },
+        ],
+      },
+    }),
+    defineArrayMember({
+        type: "image",
+        icon: ImageIcon,
+        options: {hotspot: true},
+        fields: [{ name: "alt", type: "string", title: "Alternative Text"}]
+    })
+  ],
+});
